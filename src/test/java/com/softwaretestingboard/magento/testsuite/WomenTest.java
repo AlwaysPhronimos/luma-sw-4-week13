0e3719dbc862d0dac8958d1e932506539e23fb61
jpatel2000 <jugnu.patel2000@gmail.com>
2023-12-03 23:02:36 +0000

    lume-sw-4

---
+++ src/test/java/com/softwaretestingboard/magento/testsuite/WomenTest.java
@@ -0,0 +1,53 @@
+package com.softwaretestingboard.magento.testsuite;
+
+import com.softwaretestingboard.magento.pages.HomePage;
+import com.softwaretestingboard.magento.pages.ProductPage;
+import com.softwaretestingboard.magento.pages.ShoppingCart;
+import com.softwaretestingboard.magento.testbase.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class WomenTest extends TestBase {
+    HomePage homePage = new HomePage();
+    ProductPage productPage = new ProductPage();
+    ShoppingCart shoppingCart = new ShoppingCart();
+
+    @Test
+    public void userShouldAddProductSuccessFullyToShoppingCart() throws InterruptedException {
+
+        // Mouse Hover on Gear Menu
+        Thread.sleep(2000);
+        homePage.mouseHoverOnGearTab();
+        Thread.sleep(2000);
+        // Click on Bags
+        homePage.mouseHoverAndClickOnBagsInGear();
+        Thread.sleep(2000);
+        // Click on Product Name ‘Overnight Duffle’
+        productPage.clickOnProductOvernightDuffle();
+        // Verify the text ‘Overnight Duffle’
+        Assert.assertEquals(productPage.verifyOvernightDuffleText(),"Overnight Duffle");
+        // Change Qty 3
+        productPage.changeQty("3");
+        // Click on ‘Add to Cart’ Button.
+        productPage.clickOnAddToCartButton();
+        // Verify the text ‘You added Overnight Duffle to your shopping cart.’
+        Assert.assertEquals(productPage.verifyProductAddedToCartMessage(),"You added Overnight Duffle to your shopping cart.");
+        // Click on ‘shopping cart’ Link into message
+        productPage.clickOnShoppingCartLink();
+        // Verify the product name ‘Overnight Duffle’
+        Assert.assertEquals(shoppingCart.verifyProductNameOvernightDuffle(),"Overnight Duffle");
+        //Verify the Qty is ‘3’
+        Assert.assertEquals(shoppingCart.verifyProductOvernightDufflePriceText(),"3");
+        // Verify the product price ‘$135.00’
+        Assert.assertEquals(shoppingCart.verifyProductOvernightDufflePriceText(),"$135.00");
+        // Change Qty to ‘5’
+        shoppingCart.changeFieldInCart("5");
+        Thread.sleep(2000);
+        // Click on ‘Update Shopping Cart’ button
+        shoppingCart.updateShoppingCart();
+        // Verify the product price ‘$225.00’
+        Thread.sleep(1000);
+        Assert.assertEquals(shoppingCart.verifyProductPrice(),"$225.00");
+
+    }
+}
